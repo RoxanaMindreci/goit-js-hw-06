4733649a4e55fc4f5b08dced103cd5b5b26ac519
RoxanaMindreci <142803297+RoxanaMindreci@users.noreply.github.com>
2024-01-07 13:55:36 +0200

    solving task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -32,4 +32,41 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  const inputElement = document.querySelector('#controls input');
+  const createButton = document.querySelector('[data-create]');
+  const destroyButton = document.querySelector('[data-destroy]');
+  const boxesContainer = document.querySelector('#boxes');
+
+  createButton.addEventListener('click', function () {
+    const amount = parseInt(inputElement.value);
+    createBoxes(amount);
+  });
+
+
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = '';
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.background = getRandomHexColor();
+    boxesContainer.appendChild(box);
+
+    size += 10;
+
+  }
 }
+
+ function destroyBoxes() {
+  boxes.container.innerHtml = '';
+ }
+  
+});
